df22a2e2d57b329fd987e44805abe4400dd95c8f
niiidzzz <Nandini.37614@stu.upes.ac.in>
2026-09-04 10:29:17 +0530

    Add files via upload

---
+++ day 26 2nd.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main () {
+int groups[] = {1,3,5,3,1};
+int i,j;
+for (i = 0 ; i < 5; i++){
+for (j =0; j< groups[i]; j++) {
+printf("*\n");
+}
+printf("\n");
+}
+return 0;
+}
