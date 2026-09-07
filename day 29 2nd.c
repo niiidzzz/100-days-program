cd28445c96e407f2fc8c4b62c8eece0dee11bdf2
niiidzzz <Nandini.37614@stu.upes.ac.in>
2026-09-07 21:42:21 +0530

    Add files via upload

---
+++ day 29 2nd.c
@@ -0,0 +1,27 @@
+int main() {
+    int arr[100], n, max, min;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    max = arr[0];
+    min = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max)
+            max = arr[i];
+
+        if (arr[i] < min)
+            min = arr[i];
+    }
+
+    printf("Maximum element = %d\n", max);
+    printf("Minimum element = %d\n", min);
+
+    return 0;
+}
