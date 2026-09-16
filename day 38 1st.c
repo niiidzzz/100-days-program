fa31202ff6e7a830197abda7404b503008941546
niiidzzz <Nandini.37614@stu.upes.ac.in>
2026-09-16 15:46:49 +0530

    Add files via upload

---
+++ day 38 1st.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[2][2], b[2][2], sum[2][2];
+    int i, j;
+
+    // Input first matrix
+    printf("Enter elements of first matrix:\n");
+    for(i = 0; i < 2; i++)
+    {
+        for(j = 0; j < 2; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Input second matrix
+    printf("Enter elements of second matrix:\n");
+    for(i = 0; i < 2; i++)
+    {
+        for(j = 0; j < 2; j++)
+        {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    // Add matrices
+    for(i = 0; i < 2; i++)
+    {
+        for(j = 0; j < 2; j++)
+        {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    // Print result
+    printf("Sum of matrices:\n");
+    for(i = 0; i < 2; i++)
+    {
+        for(j = 0; j < 2; j++)
+        {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
